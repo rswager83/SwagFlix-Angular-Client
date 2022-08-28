c90569f989f4c179c885258846e2836686a5bbb2
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-08-28 11:40:59 +0200

    User-Update Component
    Creating a user-update component.

---
+++ src/app/user-update/user-update.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-update',
+  templateUrl: './user-update.component.html',
+  styleUrls: ['./user-update.component.scss']
+})
+export class UserUpdateComponent implements OnInit {
+  
+  @Input() userData: any = {};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackbar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserUpdateComponent>
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  updateAppUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((resp:any) => {
+      console.log(resp);
+      this.dialogRef.close();
+      this.snackbar.open('Your profile has been updated', 'ok', {
+        duration: 3000
+      });
+    })
+  }
+  
+}
