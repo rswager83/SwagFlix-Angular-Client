61bf08137bafbee17dcf8d33b0a5a457042ed72a
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-08-28 11:38:58 +0200

    Profile Component
    Creating a user profile component with the options to delete the profile or to update it.

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,59 @@
+import { UserUpdateComponent } from './../user-update/user-update.component';
+import { WelcomePageComponent } from './../welcome-page/welcome-page.component';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  user: any = {};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public dialog: MatDialog,
+    public snackbar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+    this.user = resp;
+    console.log(this.user);
+    return this.user;
+    });
+  }
+
+  openUpdateUserDialog(): void {
+    this.dialog.open(UserUpdateComponent, {
+      width: '440px'
+    })
+  }
+
+  deleteUser(): void {
+    if (
+      confirm("Are you sure?")
+    ) {
+      this.router.navigate(['welcome']);
+      this.snackbar.open('Your profile has been deleted', 'ok', {
+        duration: 3000
+      });
+    }
+    {
+      this.fetchApiData.delUser().subscribe((result) => {
+        console.log(result)
+        localStorage.clear();
+      });
+    }
+  }
+}
